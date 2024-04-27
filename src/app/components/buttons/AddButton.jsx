e1a2eee01f2db91e4b51526bc25d78bfacbd7d6d
AndresLeOrozco <andres.leon.orozco@est.una.ac.cr>
2024-04-27 13:26:36 -0600

    Revert "changes in add and edit session"
    This reverts commit e4cef8d6d8e899e3ab3df9ab407de93ebe12db10.

--- src/app/components/buttons/AddButton.jsx
+++ src/app/components/buttons/AddButton.jsx
@@ -2,7 +2,6 @@
 import { useState } from "react";
 import FormSession from "../pop-up/FormSession";
 import FormAgreement from "../pop-up/FormAgreement";
-import SessionModal from "../pop-up/SessionModal";
 
 export const AddButton = ({ title, children, idSession = "" }) => {
   const [openModal, setOpenModal] = useState(false);
@@ -21,7 +20,7 @@ export const AddButton = ({ title, children, idSession = "" }) => {
         {children}
       </button>
       {title === "session" ? (
-        <SessionModal isModalOpen={openModal} handleModalState={handleClick} />
+        <FormSession isModalOpen={openModal} handleModalState={handleClick} />
       ) : (
         <FormAgreement
           isModalOpen={openModal}
