15427ee38db7f8cd380a00eb7af71445d624f93b
LuisLopez <lilc1105@gmail.com>
2024-04-27 01:38:18 -0600

    Revert "Merge branch 'main' of https://github.com/LuisLopezC99/S_A_A"
    This reverts commit 1e5aeeb1e349111eae762696f87b0b0cf08f38fc, reversing
    changes made to 57e908107499789488eb89716c1ddd5f274985f8.

--- src/libs/__mocks__/prisma.js
+++
@@ -1,9 +0,0 @@
-import { beforeEach } from 'vitest'
-import { mockDeep, mockReset } from 'vitest-mock-extended'
-
-beforeEach(() => {
-  mockReset(prisma)
-})
-
-const prisma = mockDeep()
-export default prisma
