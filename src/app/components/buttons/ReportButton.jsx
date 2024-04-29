0aa60e6635dd9cdfd6713ba9d30194ffae2b2a2a
Jose0696 <jose96seque@gmail.com>
2024-04-29 01:24:29 -0600

    Update ReportButton.jsx

--- src/app/components/buttons/ReportButton.jsx
+++ src/app/components/buttons/ReportButton.jsx
@@ -1,15 +1,25 @@
 "use client";
 import Report from "../reports/Report";
-const ReportButton = ({rows, header, title, state,type, filter,sesion}) => {
-    const handleClick = () => {
-      console.log(rows);
-    Report({ rows, header, title,state,type, filter,sesion });
-    
+const ReportButton = ({ rows, header, title, state, type, filter, sesion }) => {
+  const handleClick = () => {
+    console.log(rows);
+    Report({ rows, header, title, state, type, filter, sesion });
+
   };
   return (
-    <button  onClick={handleClick}>
-      Generar Reporte PDF
-    </button>
+    <div className="flex items-center justify-end">
+      {/* Aquí puedes agregar el elemento al lado del botón */}
+      <div className="mr-5">
+        {/* Contenido al lado del botón */}
+      </div>
+      <div className="relative" style={{ top: "-10px" }}>
+        <div className="flex items-center justify-end">
+          <button onClick={handleClick} className="bg-yellow-300 hover:bg-yellow-400 text-gray-500 font-bold py-2 px-4 border border-yellow-400 rounded">
+            Generar Reporte PDF
+          </button>
+        </div>
+      </div>
+    </div>
   );
 };
 
