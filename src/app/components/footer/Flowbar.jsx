924b0122d987e84f73861a895f265fbc67814375
Jose0696 <jose96seque@gmail.com>
2024-05-06 21:15:45 -0600

    Footer
    Nuevo componente para el footer

---
+++ src/app/components/footer/Flowbar.jsx
@@ -0,0 +1,24 @@
+import { getServerSession } from 'next-auth/next';
+import { authOptions } from '@/app/api/auth/[...nextauth]/route'; // Ajusta la ruta según tu estructura
+
+const Flowbar = async () => {
+    const session = await getServerSession(authOptions);
+
+    if (!session) return null;
+
+    return (
+        <footer className="bg-gray-300 border-gray-200 dark:border-gray-600 dark:bg-gray-900">
+            <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
+                <a
+                    href={session.user.role === 'admin' ? '/admin' : '/home'}
+                    className="flex items-center pr-5 md:pr-15"
+                >
+                    <p>©2024 Todos los derechos reservados por Municipalidad de Tibás. Diseño por universidad nacional escuela de informática</p>
+                </a>
+            </div>
+        </footer>
+
+    );
+};
+
+export default Flowbar;
