1004fa01598276ca46a34209be6a8a51d9ca8629
Jose0696 <jose96seque@gmail.com>
2024-05-09 22:57:58 -0600

    Comentario
    Comentario de los autores

--- src/app/components/body/pagination/Pagination.jsx
+++ src/app/components/body/pagination/Pagination.jsx
@@ -1,3 +1,39 @@
+/**
+ * Proyect: Sistema de acuerdos y actas municipales (SAA)
+ * Company: Municipalidad de Tibás
+ * Universidad Nacional de Costa Rica
+ * School of Informatic
+ * Information Systems Engineering
+ * 
+ * Date: 10/05/2024
+ * Principal_Directory: src
+ * Directory_1: app
+ * Directory: components
+ * File_1: body
+ * File_2: pagination
+ * Archive: Pagination.jsx
+ * 
+ * Authors:
+ * - Scrum: Luis Ignacio López Castro
+ *   - email: luis.lopez.castro@est.una.ac.cr
+ *   - ID: 402420889
+ * - David Coto Solano
+ *   - email: victor.coto.solano@est.una.ac.cr
+ *   - ID: 305440064
+ * - Andrés León Orozco
+ *   - email: andres.leon.orozco@est.una.ac.cr
+ *   - ID: 118920778
+ * - Eduardo Aarón Ojeda Paladino
+ *   - email: eduardo.ojeda.paladino@est.una.ac.cr
+ *   - ID: 116500136
+ * - Jennifer Quirós Chacón
+ *   - email: jennifer.quiros.chacon@est.una.ac.cr
+ *   - ID: 702790153
+ * - José Miguel Sequeira Hernández
+ *   - email: jose.sequeira.hernandez@est.una.ac.cr
+ *   - ID: 116590034
+ */
+
 "use client";
 import { useState } from "react";
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/20/solid";
--- src/app/components/pop-up/FormAgreement.jsx
+++ src/app/components/pop-up/FormAgreement.jsx
@@ -1,3 +1,38 @@
+/**
+ * Proyect: Sistema de acuerdos y actas municipales (SAA)
+ * Company: Municipalidad de Tibás
+ * Universidad Nacional de Costa Rica
+ * School of Informatic
+ * Information Systems Engineering
+ * 
+ * Date: 10/05/2024
+ * Principal_Directory: src
+ * Directory_1: app
+ * Directory: components
+ * File: pop-up
+ * Archive: FormAgreement.jsx
+ * 
+ * Authors:
+ * - Scrum: Luis Ignacio López Castro
+ *   - email: luis.lopez.castro@est.una.ac.cr
+ *   - ID: 402420889
+ * - David Coto Solano
+ *   - email: victor.coto.solano@est.una.ac.cr
+ *   - ID: 305440064
+ * - Andrés León Orozco
+ *   - email: andres.leon.orozco@est.una.ac.cr
+ *   - ID: 118920778
+ * - Eduardo Aarón Ojeda Paladino
+ *   - email: eduardo.ojeda.paladino@est.una.ac.cr
+ *   - ID: 116500136
+ * - Jennifer Quirós Chacón
+ *   - email: jennifer.quiros.chacon@est.una.ac.cr
+ *   - ID: 702790153
+ * - José Miguel Sequeira Hernández
+ *   - email: jose.sequeira.hernandez@est.una.ac.cr
+ *   - ID: 116590034
+ */
+
 "use client";
 import React, { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
